132dc61e88537e697345dace40100c136293590a
giavytday <giavytday@gmail.com>
2026-02-11 21:17:30 -0500

    Saving login code

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,7 +15,7 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+  const username = req.body.username;
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,10 +21,10 @@ public_users.post("/register", (req,res) => {
   });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', function (req, res) {
+    // Use JSON.stringify to format the output nicely with 4 spaces of indentation
+    return res.send(JSON.stringify(books, null, 4));
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
